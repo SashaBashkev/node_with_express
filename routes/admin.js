34c8174365c428d20c458704530dfc502eb1d426
Sasha Bashkev <bashkev88@gmail.com>
2019-12-04 00:06:36 +0300

    Add new routes: -done;

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const productsCtrl = require("../controllers/products");
-
-router.get("/add-product", productsCtrl.getAddProduct);
-router.post("/add-product", productsCtrl.postAddProduct);
+const adminCtrl = require("../controllers/admin");
+// GET /admin/add-product
+router.get("/add-product", adminCtrl.getAddProduct);
+// GET /admin/products
+router.get("/products", adminCtrl.getProducts);
+// POST /admin/add-products
+router.post("/add-product", adminCtrl.postAddProduct);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const productsCtrl = require("../controllers/products");
+const shopCtrl = require("../controllers/shop");
+// GET /
+router.get("/", shopCtrl.getIndex);
+// GET /
+router.get("/products", shopCtrl.getProducts);
+router.get("/cart", shopCtrl.getCart);
+router.get("/checkout", shopCtrl.getCheckout);
 
-router.get("/", productsCtrl.getProducts);
 module.exports = router;
